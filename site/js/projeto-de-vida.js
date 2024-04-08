2038bc9ad0d5e315bdf6aa0095ccf0ac9bcce75e
vitorlescola <158179013+vitorlescola@users.noreply.github.com>
2024-04-07 22:35:16 -0300

    Add files via upload

---
+++ site/js/projeto-de-vida.js
@@ -0,0 +1,108 @@
+window.addEventListener("DOMContentLoaded",() => {
+	const ctl = new CollapsibleTimeline("#timeline");
+});
+
+class CollapsibleTimeline {
+	constructor(el) {
+		this.el = document.querySelector(el);
+
+		this.init();
+	}
+	init() {
+		this.el?.addEventListener("click",this.itemAction.bind(this));
+	}
+	animateItemAction(button,ctrld,contentHeight,shouldCollapse) {
+		const expandedClass = "timeline__item-body--expanded";
+		const animOptions = {
+			duration: 300,
+			easing: "cubic-bezier(0.65,0,0.35,1)"
+		};
+
+		if (shouldCollapse) {
+			button.ariaExpanded = "false";
+			ctrld.ariaHidden = "true";
+			ctrld.classList.remove(expandedClass);
+			animOptions.duration *= 2;
+			this.animation = ctrld.animate([
+				{ height: `${contentHeight}px` },
+				{ height: `${contentHeight}px` },
+				{ height: "0px" }
+			],animOptions);
+		} else {
+			button.ariaExpanded = "true";
+			ctrld.ariaHidden = "false";
+			ctrld.classList.add(expandedClass);
+			this.animation = ctrld.animate([
+				{ height: "0px" },
+				{ height: `${contentHeight}px` }
+			],animOptions);
+		}
+	}
+	itemAction(e) {
+		const { target } = e;
+		const action = target?.getAttribute("data-action");
+		const item = target?.getAttribute("data-item");
+
+		if (action) {
+			const targetExpanded = action === "expand" ? "false" : "true";
+			const buttons = Array.from(this.el?.querySelectorAll(`[aria-expanded="${targetExpanded}"]`));
+			const wasExpanded = action === "collapse";
+
+			for (let button of buttons) {
+				const buttonID = button.getAttribute("data-item");
+				const ctrld = this.el?.querySelector(`#item${buttonID}-ctrld`);
+				const contentHeight = ctrld.firstElementChild?.offsetHeight;
+
+				this.animateItemAction(button,ctrld,contentHeight,wasExpanded);
+			}
+
+		} else if (item) {
+			const button = this.el?.querySelector(`[data-item="${item}"]`);
+			const expanded = button?.getAttribute("aria-expanded");
+
+			if (!expanded) return;
+
+			const wasExpanded = expanded === "true";
+			const ctrld = this.el?.querySelector(`#item${item}-ctrld`);
+			const contentHeight = ctrld.firstElementChild?.offsetHeight;
+
+			this.animateItemAction(button,ctrld,contentHeight,wasExpanded);
+		}
+	}
+}
+
+document.getElementById('formulario').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    var nome = document.getElementById('nome').value;
+    var sobrenome = document.getElementById('sobrenome').value;
+    var dataNascimento = document.getElementById('dataNascimento').value;
+    var localNascimento = document.getElementById('localNascimento').value;
+	var pais = document.getElementById('pais').value;
+	var familia = document.getElementById('familia').value;
+	var casa = document.getElementById('casa').value;
+	var habilidades = document.getElementById('habilidades').value;
+	var pontos = document.getElementById('pontos').value;
+	var futuro = document.getElementById('futuro').value;
+	var profissao = document.getElementById('profissao').value;
+	var gosto = document.getElementById('gosto').value;
+
+    var formData = {
+        nome: nome,
+        sobrenome: sobrenome,
+        dataNascimento: dataNascimento,
+        localNascimento: localNascimento,
+		pais : pais,
+		familia : familia,
+		casa : casa,
+		habilidades : habilidades,
+		pontos : pontos,
+		futuro : futuro,
+		profissao : profissao,
+		gosto : gosto
+    };
+
+    localStorage.setItem('formularioData', JSON.stringify(formData));
+
+    alert('Informações salvas com sucesso!');
+});
