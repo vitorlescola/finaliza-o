c6506b86ea6fef4535a26e52185584ce6d068337
vitorlescola <158179013+vitorlescola@users.noreply.github.com>
2024-04-29 11:41:11 -0300

    Add files via upload

--- site/js/financeiro.js
+++ site/js/financeiro.js
@@ -103,3 +103,14 @@ function submitQuiz() {
 	} 
 	document.getElementById('result').innerText = 'O tipo de investimento para você é:  ' + result;
 }
+function calculateCashback() {  //função que calcula o cashback
+    var quantidade = parseFloat(document.getElementById("quantidade").value);
+    var porcentagem = parseFloat(document.getElementById("porcentagem").value);
+
+    if (isNaN(quantidade) || isNaN(porcentagem)) {
+        document.getElementById("result").innerHTML = "Porfavor insira valores validos.";
+    } else {
+        var cashback = (quantidade * porcentagem) / 100;
+        document.getElementById("result").innerHTML = "Cashback: R$" + cashback.toFixed(2);
+    }
+}
